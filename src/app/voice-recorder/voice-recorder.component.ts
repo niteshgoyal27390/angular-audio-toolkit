946bb69cfc855c1f46b5c8322844c1b2a088022d
Nitesh Goyal <ngoyal@cloudlex.com>
2024-02-21 15:47:42 +0100

    Added the logic to record the voice using browser APIs

--- src/app/voice-recorder/voice-recorder.component.ts
+++ src/app/voice-recorder/voice-recorder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-voice-recorder',
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoiceRecorderComponent implements OnInit {
 
+  @ViewChild('audioPlayer') audioPlayer: ElementRef;
+
+  mediaRecorder: MediaRecorder;
+  chunks: Blob[] = [];
+  isRecording: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  startRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        this.mediaRecorder = new MediaRecorder(stream);
+
+        this.mediaRecorder.ondataavailable = (e: any) => {
+          this.chunks.push(e.data);
+        };
+
+        this.mediaRecorder.onstop = () => {
+          const blob = new Blob(this.chunks, { type: 'audio/mp3' });
+          this.chunks = [];
+          const audioURL = window.URL.createObjectURL(blob);
+          this.audioPlayer.nativeElement.src = audioURL;
+        };
+
+        this.mediaRecorder.start();
+        this.isRecording = true;
+      })
+      .catch(error => {
+        console.error('Error accessing the microphone: ', error);
+      });
+  }
+
+  pauseRecording() {
+    if (this.mediaRecorder.state === 'recording') {
+      this.mediaRecorder.pause();
+      this.isRecording = false;
+    }
+  }
+
+  resumeRecording() {
+    if (this.mediaRecorder.state === 'paused') {
+      this.mediaRecorder.resume();
+      this.isRecording = true;
+    }
+  }
+
+  stopRecording() {
+    if (this.mediaRecorder.state !== 'inactive') {
+      this.mediaRecorder.stop();
+      this.isRecording = false;
+    }
   }
 
 }
