078347c526bdae80cac5aabaa2eb3ac77626c3c6
Nitesh Goyal <ngoyal@cloudlex.com>
2024-02-21 15:43:31 +0100

    Added new child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VoiceRecorderComponent } from './voice-recorder/voice-recorder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoiceRecorderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/voice-recorder/voice-recorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voice-recorder',
+  templateUrl: './voice-recorder.component.html',
+  styleUrls: ['./voice-recorder.component.scss']
+})
+export class VoiceRecorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
