53fe207efd8ccb515e02352fe2f0446e66219555
Nitesh Goyal <ngoyal@cloudlex.com>
2024-03-13 22:04:34 +0100

    Fixed Safari browser not supporting issue and added some checks

--- src/app/voice-recorder/voice-recorder.component.ts
+++ src/app/voice-recorder/voice-recorder.component.ts
@@ -9,66 +9,90 @@ export class VoiceRecorderComponent implements OnInit {
 
   @ViewChild('audioPlayer') audioPlayer: ElementRef;
 
+  mediaStream: MediaStream;
   mediaRecorder: MediaRecorder;
   chunks: Blob[] = [];
   isRecording: boolean = false;
+  isPaused: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-    
+  async ngOnInit() {
+    try {
+      this.mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      // this.audioPlayer.nativeElement.srcObject = this.mediaStream;
+    } catch (error) {
+      console.error('Error accessing the microphone: ', error);
+    }
   }
 
   startRecording() {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        this.mediaRecorder = new MediaRecorder(stream);
+    try {
+      if (!this.isPaused) {
+        this.mediaRecorder = new MediaRecorder(this.mediaStream);
 
-        this.mediaRecorder.ondataavailable = (e: any) => {
-          this.chunks.push(e.data);
+        this.mediaRecorder.ondataavailable = (e: BlobEvent) => {
+          if (e.data.size > 0) {
+            this.chunks.push(e.data);
+            const audioBlob = new Blob(this.chunks, { type: 'audio/mp4' });
+            const audioURL = URL.createObjectURL(audioBlob);
+            this.audioPlayer.nativeElement.src = audioURL;
+          }
         };
 
         this.mediaRecorder.onstop = () => {
-          const blob = new Blob(this.chunks, { type: 'audio/mp3' });
-          this.chunks = [];
-          const audioURL = window.URL.createObjectURL(blob);
-          this.audioPlayer.nativeElement.src = audioURL;
+          if (this.chunks.length > 0) {
+            const audioBlob = new Blob(this.chunks, { type: 'audio/mp4' });
+            const audioURL = URL.createObjectURL(audioBlob);
+            this.audioPlayer.nativeElement.src = audioURL;
+          }
         };
 
-        this.mediaRecorder.onpause = () => {
-          const blob = new Blob(this.chunks, { type: 'audio/mp3' });
-          const audioURL = window.URL.createObjectURL(blob);
-          this.audioPlayer.nativeElement.src = audioURL;
-        };
+      }
 
+      if (!this.isRecording || this.isPaused) {
         this.mediaRecorder.start();
         this.isRecording = true;
-      })
-      .catch(error => {
-        console.error('Error accessing the microphone: ', error);
-      });
+        this.isPaused = false;
+      }
+    } catch(error) {
+      console.error('Error accessing the microphone: ', error);
+    };
   }
 
   pauseRecording() {
-    if (this.mediaRecorder.state === 'recording') {
+    if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
       this.mediaRecorder.requestData();
       this.mediaRecorder.pause();
       this.isRecording = false;
+      this.isPaused = true;
     }
   }
 
   resumeRecording() {
     this.audioPlayer.nativeElement.src = "";
-    if (this.mediaRecorder.state === 'paused') {
+    if (this.mediaRecorder && this.mediaRecorder.state === 'paused') {
       this.mediaRecorder.resume();
       this.isRecording = true;
+      this.isPaused = false;
     }
   }
 
   stopRecording() {
-    if (this.mediaRecorder.state !== 'inactive') {
+    if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
       this.mediaRecorder.stop();
       this.isRecording = false;
+      this.isPaused = false;
+    }
+  }
+
+  ngOnDestroy() {
+    // Clean up resources when the component is destroyed
+    if (this.mediaStream) {
+      this.mediaStream.getTracks().forEach(track => track.stop());
+    }
+    if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
+      this.mediaRecorder.stop();
     }
   }
 
